bc5a45dab8ede8b74970499d9a5a55bf4864295b
Tragg7 <umartem2016@gmail.com>
2025-06-04 23:59:08 +0300

    feat: last last commit

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,3 @@ urlpatterns = [
     path('', include('main.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
